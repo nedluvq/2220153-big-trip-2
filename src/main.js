c4b84e4791c7d6ad66097b6a6e3978e34604a0d0
nedluvq <vasav7001@gmail.com>
2023-03-06 18:35:23 +0500

    added presenter and render parts by js

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { render, RenderPosition} from './view/render';
+import Filter from './view/filters';
+import Trip from './presenter/trip';
+
+const filterContainer = document.querySelector('.trip-controls__filters');
+const tripContainer = document.querySelector('.trip-events');
+const tripPresenter = new Trip({container: tripContainer});
+
+render(new Filter(), filterContainer, RenderPosition.BEFOREEND);
+tripPresenter.init();
---
+++ src/view/events-view.js
@@ -0,0 +1,24 @@
+import { createElement } from '../render.js';
+
+const createEventsTemplate = () => (
+  `<ul class="trip-events__list">
+  </ul>`
+);
+
+export default class EventsView {
+  getTemplate () {
+    return createEventsTemplate;
+  }
+
+  getElement() {
+    if (!this.element){
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
